d305e3e72c7d63f252351af57b605e80a1abc610
adham-suwan <a.suwan@msn.com>
2019-01-23 12:20:07 +0200

    error cmt

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -11,9 +11,10 @@ namespace WebApplication1
     //develop branch
     public class MvcApplication : System.Web.HttpApplication
     {
+        //latest
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas()
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
